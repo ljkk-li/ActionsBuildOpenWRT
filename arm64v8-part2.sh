fb25037e3f29a645a11df54300001c883334cf5f
ljkk-li <51225555+ljkk-li@users.noreply.github.com>
2024-05-25 07:34:06 +0800

    Update arm64v8-part2.sh

--- arm64v8-part2.sh
+++ arm64v8-part2.sh
@@ -13,5 +13,5 @@ git sparse-checkout set luci-app-openclash
 cd ../..
 
 #readd cpufreq for aarch64
-sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
-sed -i 's/services/system/g'  package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+#sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+#sed -i 's/services/system/g'  package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
